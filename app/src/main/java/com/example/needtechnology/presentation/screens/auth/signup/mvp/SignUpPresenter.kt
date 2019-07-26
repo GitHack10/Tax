1bd4bc43831cee604a5f57863610b59604dc7d4d
Kamil Abdulatipov <abdulatipov.k97@gmail.com>
2019-07-26 04:46:17 +0300

    изменил дизайн на экране "Регистрация"

--- app/src/main/java/com/example/needtechnology/presentation/screens/auth/signup/mvp/SignUpPresenter.kt
+++ app/src/main/java/com/example/needtechnology/presentation/screens/auth/signup/mvp/SignUpPresenter.kt
@@ -3,6 +3,7 @@ package com.example.needtechnology.presentation.screens.auth.signup.mvp
 import com.example.needtechnology.di.global.nameds.SIGN_UP_FLOW
 import com.example.needtechnology.domain.auth.AuthInteractor
 import com.example.needtechnology.domain.global.UserInfo
+import com.example.needtechnology.presentation.global.Screens
 import com.example.needtechnology.presentation.global.base.BasePresenter
 import com.example.needtechnology.presentation.global.navigation.FlowRouter
 import javax.inject.Inject
@@ -14,8 +15,10 @@ class SignUpPresenter@Inject constructor(
     private var userInfo: UserInfo
 ): BasePresenter<SignUpView>(flowRouter) {
 
-    fun onDoneClicked() {
-
+    fun onDoneClicked(userInfo: UserInfo) {
+        interactor.saveUserInfo(userInfo)
+        interactor.setIsLogin(true)
+        flowRouter.newRootFlow(Screens.MainFlow())
     }
 
     override fun onBackPressed() = flowRouter.exitFlow()
--- app/src/main/java/com/example/needtechnology/presentation/screens/auth/signup/ui/SignUpFragment.kt
+++ app/src/main/java/com/example/needtechnology/presentation/screens/auth/signup/ui/SignUpFragment.kt
@@ -1,29 +1,42 @@
 package com.example.needtechnology.presentation.screens.auth.signup.ui
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.example.needtechnology.R
 import com.example.needtechnology.di.global.nameds.SIGN_UP_FLOW
+import com.example.needtechnology.domain.global.UserInfo
 import com.example.needtechnology.presentation.global.base.FlowFragment
+import com.example.needtechnology.presentation.global.utils.accessible
+import com.example.needtechnology.presentation.global.utils.hideKeyboard
 import com.example.needtechnology.presentation.global.utils.setWhiteStyleWindow
 import com.example.needtechnology.presentation.screens.auth.signup.mvp.SignUpPresenter
 import com.example.needtechnology.presentation.screens.auth.signup.mvp.SignUpView
+import com.jakewharton.rxbinding2.widget.RxTextView
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.AndroidSupportInjection
 import dagger.android.support.HasSupportFragmentInjector
+import io.reactivex.rxkotlin.Observables
+import io.reactivex.rxkotlin.plusAssign
+import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.fragment_sign_up.*
 import ru.terrakok.cicerone.NavigatorHolder
 import ru.terrakok.cicerone.android.support.SupportAppNavigator
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 import javax.inject.Named
 
-class SignUpFragment : FlowFragment(), SignUpView, HasSupportFragmentInjector, View.OnClickListener {
-    override val container = R.id.registration_container
+class SignUpFragment : FlowFragment(), SignUpView, HasSupportFragmentInjector,
+    View.OnClickListener {
     override val layoutRes = R.layout.fragment_sign_up
+    override val container = R.id.registration_container
 
     @Inject
     @field:Named(SIGN_UP_FLOW)
@@ -41,6 +54,11 @@ class SignUpFragment : FlowFragment(), SignUpView, HasSupportFragmentInjector, V
     @ProvidePresenter
     fun providePresenter() = presenter
 
+    @SuppressLint("SimpleDateFormat")
+    private val dateSendFormat = SimpleDateFormat("yyyy-MM-dd")
+    @SuppressLint("SimpleDateFormat")
+    private val dateFormat = SimpleDateFormat("dd MMMM yyyy")
+
     override fun onAttach(context: Context?) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
@@ -55,12 +73,78 @@ class SignUpFragment : FlowFragment(), SignUpView, HasSupportFragmentInjector, V
 
     override fun onClick(view: View?) {
         when (view?.id) {
-            Button_registrationFragment_done.id -> presenter.onDoneClicked()
+            R.id.doneButton -> collectUserData()
+            R.id.birthEdit -> showDatePicker()
         }
+    }
+
+    private fun collectUserData() {
+        val userInfo = UserInfo(
+            name = usernameEdit.text.toString(),
+            email = emailEdit.text.toString(),
+            phone = phoneEdit.text.toString(),
+            birth = birthEdit.text.toString(),
+            gender = if (maleRadioButton.isChecked) "Мужской" else "Женский"
+        )
+        presenter.onDoneClicked(userInfo)
+    }
+
+    private fun showDatePicker() {
+        val editDateFormat = SimpleDateFormat("dd MM yyyy")
+        val calendar = Calendar.getInstance()
+        val day: Int?
+        val month: Int?
+        val year: Int?
+
+        if (birthEdit.text.isEmpty()) {
+            day = calendar.get(Calendar.DAY_OF_MONTH)
+            month = calendar.get(Calendar.MONTH)
+            year = calendar.get(Calendar.YEAR)
+        } else {
+            val date = dateFormat.parse(birthEdit.text.toString())
+            val userBirth = editDateFormat.format(date).split(" ")
+            day = userBirth[0].toInt()
+            month = userBirth[1].toInt() - 1
+            year = userBirth[2].toInt()
+        }
+        val datePickerDialog = DatePickerDialog(
+            context!!,
+            R.style.CustomDatePickerDialog,
+            DatePickerDialog.OnDateSetListener { _, yearUser, monthOfYear, dayOfMonth ->
+                calendar.set(yearUser, monthOfYear, dayOfMonth)
+                birthEdit.setText(dateFormat.format(calendar.timeInMillis))
+            }, year, month, day
+        )
+
+        val minDate = Calendar.getInstance()
+        minDate.add(Calendar.YEAR, -10)
+        datePickerDialog.datePicker.maxDate = minDate.timeInMillis
+        datePickerDialog.show()
     }
 
     private fun init() {
         setupToolbar(getString(R.string.menu_sign_up), true)
+
+        doneButton.setOnClickListener(this)
+        birthEdit.setOnClickListener(this)
+
+        phoneEdit.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                hideKeyboard()
+            }
+            false
+        }
+
+        subscriptions += Observables.combineLatest(
+            RxTextView.textChanges(usernameEdit),
+            RxTextView.textChanges(emailEdit),
+            RxTextView.textChanges(phoneEdit),
+            RxTextView.textChanges(birthEdit)
+        ) { username, email, phone, birth ->
+            username.isNotBlank() && email.isNotBlank()
+                && phone.isNotBlank() && birth.isNotBlank() && phoneEdit.rawText.length > 9
+        }
+            .subscribeBy { doneButton.accessible(it) }
     }
 
     override fun supportFragmentInjector() = fragmentInjector
