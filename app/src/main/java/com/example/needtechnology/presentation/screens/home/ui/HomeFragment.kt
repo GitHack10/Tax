973141111d25d3b5b00f9880a4e04d781a63429c
Kamil Abdulatipov <abdulatipov.k97@gmail.com>
2019-07-26 04:53:15 +0300

    изменил дизайн на экране "Главная"

--- app/src/main/java/com/example/needtechnology/presentation/screens/home/ui/HomeFragment.kt
+++ app/src/main/java/com/example/needtechnology/presentation/screens/home/ui/HomeFragment.kt
@@ -4,18 +4,25 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.View
+import android.widget.Toast
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.example.needtechnology.R
 import com.example.needtechnology.presentation.global.base.BaseFragment
+import com.example.needtechnology.presentation.global.utils.accessible
 import com.example.needtechnology.presentation.screens.home.mvp.HomePresenter
 import com.example.needtechnology.presentation.screens.home.mvp.HomeView
+import com.jakewharton.rxbinding2.widget.RxTextView
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.AndroidSupportInjection
 import dagger.android.support.HasSupportFragmentInjector
+import io.reactivex.rxkotlin.Observables
+import io.reactivex.rxkotlin.plusAssign
+import io.reactivex.rxkotlin.subscribeBy
+import kotlinx.android.synthetic.main.fragment_home.*
 import javax.inject.Inject
 
-class HomeFragment : BaseFragment(), HomeView, HasSupportFragmentInjector {
+class HomeFragment : BaseFragment(), HomeView, HasSupportFragmentInjector, View.OnClickListener {
     override val layoutRes = R.layout.fragment_home
 
     override fun supportFragmentInjector() = fragmentInjector
@@ -40,8 +47,41 @@ class HomeFragment : BaseFragment(), HomeView, HasSupportFragmentInjector {
         init()
     }
 
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.qrScannerTextView -> showQrScanner()
+            R.id.sendCheck -> collectCheck()
+        }
+    }
+
+    private fun showQrScanner() {
+
+    }
+
+    private fun collectCheck() {
+        Toast.makeText(context, "Чек зарегистрирован!", Toast.LENGTH_SHORT).show()
+        inputFP.text.clear()
+        inputFD.text.clear()
+        inputFN.text.clear()
+        inputDate.text.clear()
+    }
+
     private fun init() {
         setupToolbar(getString(R.string.menu_home))
+
+        qrScannerTextView.setOnClickListener(this)
+        sendCheck.setOnClickListener(this)
+
+        subscriptions += Observables.combineLatest(
+            RxTextView.textChanges(inputFP),
+            RxTextView.textChanges(inputFD),
+            RxTextView.textChanges(inputFN),
+            RxTextView.textChanges(inputDate)
+        ) { fd, fp, fn, date ->
+            fd.isNotBlank() && fp.isNotBlank()
+                && fn.isNotBlank() && date.isNotBlank()
+        }
+            .subscribeBy { sendCheck.accessible(it) }
     }
 
     override fun onBackPressed() {
